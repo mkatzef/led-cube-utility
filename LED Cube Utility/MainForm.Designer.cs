0cd284ff8fecbc12556bf75ac442da3042983d1a
Marc Katzef <marckatzef@gmail.com>
2017-09-04 13:03:58 +1200

    Added button elements to main form.

--- LED Cube Utility/MainForm.Designer.cs
+++ LED Cube Utility/MainForm.Designer.cs
@@ -31,15 +31,24 @@
 			this.mnuFileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
 			this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.mnuEditNewFrame = new System.Windows.Forms.ToolStripMenuItem();
-			this.mnuEditFrame = new System.Windows.Forms.ToolStripMenuItem();
-			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
-			this.mnuEditFillLayer = new System.Windows.Forms.ToolStripMenuItem();
-			this.mnuEditClearLayer = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuEditDeleteFrame = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.mnuToolsComPort = new System.Windows.Forms.ToolStripMenuItem();
 			this.mnuToolsBaudRate = new System.Windows.Forms.ToolStripMenuItem();
 			this.mnuToolsProgramDevice = new System.Windows.Forms.ToolStripMenuItem();
+			this.panelLayerOptions = new System.Windows.Forms.Panel();
+			this.panelLayerGrid = new System.Windows.Forms.Panel();
+			this.buttonFrameTime = new System.Windows.Forms.Button();
+			this.buttonFrameLeft = new System.Windows.Forms.Button();
+			this.buttonFrameRight = new System.Windows.Forms.Button();
+			this.labelLayerInfo = new System.Windows.Forms.Label();
+			this.buttonClearLayer = new System.Windows.Forms.Button();
+			this.buttonFillLayer = new System.Windows.Forms.Button();
+			this.buttonLayerDown = new System.Windows.Forms.Button();
+			this.buttonLayerUp = new System.Windows.Forms.Button();
+			this.labelFrameInfo = new System.Windows.Forms.Label();
 			this.menuStrip1.SuspendLayout();
+			this.panelLayerOptions.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// menuStrip1
@@ -51,7 +60,7 @@
             this.toolsToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(430, 33);
+			this.menuStrip1.Size = new System.Drawing.Size(724, 33);
 			this.menuStrip1.TabIndex = 0;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -69,28 +78,28 @@
 			// mnuFileNew
 			// 
 			this.mnuFileNew.Name = "mnuFileNew";
-			this.mnuFileNew.Size = new System.Drawing.Size(210, 30);
+			this.mnuFileNew.Size = new System.Drawing.Size(170, 30);
 			this.mnuFileNew.Text = "New";
 			this.mnuFileNew.Click += new System.EventHandler(this.mnuFileNew_Click);
 			// 
 			// mnuFileOpen
 			// 
 			this.mnuFileOpen.Name = "mnuFileOpen";
-			this.mnuFileOpen.Size = new System.Drawing.Size(210, 30);
+			this.mnuFileOpen.Size = new System.Drawing.Size(170, 30);
 			this.mnuFileOpen.Text = "Open...";
 			this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
 			// 
 			// mnuFileSave
 			// 
 			this.mnuFileSave.Name = "mnuFileSave";
-			this.mnuFileSave.Size = new System.Drawing.Size(210, 30);
+			this.mnuFileSave.Size = new System.Drawing.Size(170, 30);
 			this.mnuFileSave.Text = "Save";
 			this.mnuFileSave.Click += new System.EventHandler(this.mnuFileSave_Click);
 			// 
 			// mnuFileSaveAs
 			// 
 			this.mnuFileSaveAs.Name = "mnuFileSaveAs";
-			this.mnuFileSaveAs.Size = new System.Drawing.Size(210, 30);
+			this.mnuFileSaveAs.Size = new System.Drawing.Size(170, 30);
 			this.mnuFileSaveAs.Text = "Save As...";
 			this.mnuFileSaveAs.Click += new System.EventHandler(this.mnuFileSaveAs_Click);
 			// 
@@ -98,10 +107,7 @@
 			// 
 			this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuEditNewFrame,
-            this.mnuEditFrame,
-            this.toolStripMenuItem1,
-            this.mnuEditFillLayer,
-            this.mnuEditClearLayer});
+            this.mnuEditDeleteFrame});
 			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
 			this.editToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
 			this.editToolStripMenuItem.Text = "Edit";
@@ -110,34 +116,15 @@
 			// 
 			this.mnuEditNewFrame.Name = "mnuEditNewFrame";
 			this.mnuEditNewFrame.Size = new System.Drawing.Size(210, 30);
-			this.mnuEditNewFrame.Text = "New Frame";
+			this.mnuEditNewFrame.Text = "New Frame...";
 			this.mnuEditNewFrame.Click += new System.EventHandler(this.mnuEditNewFrame_Click);
 			// 
-			// mnuEditFrame
+			// mnuEditDeleteFrame
 			// 
-			this.mnuEditFrame.Name = "mnuEditFrame";
-			this.mnuEditFrame.Size = new System.Drawing.Size(210, 30);
-			this.mnuEditFrame.Text = "Frame";
-			this.mnuEditFrame.Click += new System.EventHandler(this.mnuEditFrame_Click);
-			// 
-			// toolStripMenuItem1
-			// 
-			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-			this.toolStripMenuItem1.Size = new System.Drawing.Size(182, 6);
-			// 
-			// mnuEditFillLayer
-			// 
-			this.mnuEditFillLayer.Name = "mnuEditFillLayer";
-			this.mnuEditFillLayer.Size = new System.Drawing.Size(210, 30);
-			this.mnuEditFillLayer.Text = "Fill Layer";
-			this.mnuEditFillLayer.Click += new System.EventHandler(this.mnuEditFillLayer_Click);
-			// 
-			// mnuEditClearLayer
-			// 
-			this.mnuEditClearLayer.Name = "mnuEditClearLayer";
-			this.mnuEditClearLayer.Size = new System.Drawing.Size(210, 30);
-			this.mnuEditClearLayer.Text = "Clear Layer";
-			this.mnuEditClearLayer.Click += new System.EventHandler(this.mnuEditClearLayer_Click);
+			this.mnuEditDeleteFrame.Name = "mnuEditDeleteFrame";
+			this.mnuEditDeleteFrame.Size = new System.Drawing.Size(210, 30);
+			this.mnuEditDeleteFrame.Text = "Delete Frame";
+			this.mnuEditDeleteFrame.Click += new System.EventHandler(this.mnuEditDeleteFrame_Click);
 			// 
 			// toolsToolStripMenuItem
 			// 
@@ -170,17 +157,137 @@
 			this.mnuToolsProgramDevice.Text = "Program Device";
 			this.mnuToolsProgramDevice.Click += new System.EventHandler(this.mnuToolsProgramDevice_Click);
 			// 
-			// MainWindow
+			// panelLayerOptions
+			// 
+			this.panelLayerOptions.Controls.Add(this.labelFrameInfo);
+			this.panelLayerOptions.Controls.Add(this.buttonClearLayer);
+			this.panelLayerOptions.Controls.Add(this.buttonFillLayer);
+			this.panelLayerOptions.Controls.Add(this.buttonLayerDown);
+			this.panelLayerOptions.Controls.Add(this.buttonLayerUp);
+			this.panelLayerOptions.Controls.Add(this.labelLayerInfo);
+			this.panelLayerOptions.Controls.Add(this.buttonFrameRight);
+			this.panelLayerOptions.Controls.Add(this.buttonFrameLeft);
+			this.panelLayerOptions.Controls.Add(this.buttonFrameTime);
+			this.panelLayerOptions.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panelLayerOptions.Location = new System.Drawing.Point(0, 33);
+			this.panelLayerOptions.Name = "panelLayerOptions";
+			this.panelLayerOptions.Size = new System.Drawing.Size(112, 586);
+			this.panelLayerOptions.TabIndex = 1;
+			// 
+			// panelLayerGrid
+			// 
+			this.panelLayerGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelLayerGrid.Location = new System.Drawing.Point(112, 33);
+			this.panelLayerGrid.Name = "panelLayerGrid";
+			this.panelLayerGrid.Size = new System.Drawing.Size(612, 586);
+			this.panelLayerGrid.TabIndex = 3;
+			// 
+			// buttonFrameTime
+			// 
+			this.buttonFrameTime.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.buttonFrameTime.Location = new System.Drawing.Point(0, 535);
+			this.buttonFrameTime.Name = "buttonFrameTime";
+			this.buttonFrameTime.Size = new System.Drawing.Size(112, 51);
+			this.buttonFrameTime.TabIndex = 8;
+			this.buttonFrameTime.Text = "Frame Time";
+			this.buttonFrameTime.UseVisualStyleBackColor = true;
+			// 
+			// buttonFrameLeft
+			// 
+			this.buttonFrameLeft.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.buttonFrameLeft.Location = new System.Drawing.Point(0, 484);
+			this.buttonFrameLeft.Name = "buttonFrameLeft";
+			this.buttonFrameLeft.Size = new System.Drawing.Size(112, 51);
+			this.buttonFrameLeft.TabIndex = 9;
+			this.buttonFrameLeft.Text = "Frame Left";
+			this.buttonFrameLeft.UseVisualStyleBackColor = true;
+			// 
+			// buttonFrameRight
+			// 
+			this.buttonFrameRight.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.buttonFrameRight.Location = new System.Drawing.Point(0, 433);
+			this.buttonFrameRight.Name = "buttonFrameRight";
+			this.buttonFrameRight.Size = new System.Drawing.Size(112, 51);
+			this.buttonFrameRight.TabIndex = 10;
+			this.buttonFrameRight.Text = "Frame Right";
+			this.buttonFrameRight.UseVisualStyleBackColor = true;
+			// 
+			// labelLayerInfo
+			// 
+			this.labelLayerInfo.AutoSize = true;
+			this.labelLayerInfo.Dock = System.Windows.Forms.DockStyle.Top;
+			this.labelLayerInfo.Location = new System.Drawing.Point(0, 0);
+			this.labelLayerInfo.Name = "labelLayerInfo";
+			this.labelLayerInfo.Size = new System.Drawing.Size(48, 20);
+			this.labelLayerInfo.TabIndex = 11;
+			this.labelLayerInfo.Text = "Layer";
+			// 
+			// buttonClearLayer
+			// 
+			this.buttonClearLayer.Dock = System.Windows.Forms.DockStyle.Top;
+			this.buttonClearLayer.Location = new System.Drawing.Point(0, 173);
+			this.buttonClearLayer.Name = "buttonClearLayer";
+			this.buttonClearLayer.Size = new System.Drawing.Size(112, 51);
+			this.buttonClearLayer.TabIndex = 15;
+			this.buttonClearLayer.Text = "Clear Layer";
+			this.buttonClearLayer.UseVisualStyleBackColor = true;
+			// 
+			// buttonFillLayer
+			// 
+			this.buttonFillLayer.Dock = System.Windows.Forms.DockStyle.Top;
+			this.buttonFillLayer.Location = new System.Drawing.Point(0, 122);
+			this.buttonFillLayer.Name = "buttonFillLayer";
+			this.buttonFillLayer.Size = new System.Drawing.Size(112, 51);
+			this.buttonFillLayer.TabIndex = 14;
+			this.buttonFillLayer.Text = "Fill Layer";
+			this.buttonFillLayer.UseVisualStyleBackColor = true;
+			// 
+			// buttonLayerDown
+			// 
+			this.buttonLayerDown.Dock = System.Windows.Forms.DockStyle.Top;
+			this.buttonLayerDown.Location = new System.Drawing.Point(0, 71);
+			this.buttonLayerDown.Name = "buttonLayerDown";
+			this.buttonLayerDown.Size = new System.Drawing.Size(112, 51);
+			this.buttonLayerDown.TabIndex = 13;
+			this.buttonLayerDown.Text = "Layer Down";
+			this.buttonLayerDown.UseVisualStyleBackColor = true;
+			// 
+			// buttonLayerUp
+			// 
+			this.buttonLayerUp.Dock = System.Windows.Forms.DockStyle.Top;
+			this.buttonLayerUp.Location = new System.Drawing.Point(0, 20);
+			this.buttonLayerUp.Name = "buttonLayerUp";
+			this.buttonLayerUp.Size = new System.Drawing.Size(112, 51);
+			this.buttonLayerUp.TabIndex = 12;
+			this.buttonLayerUp.Text = "Layer Up";
+			this.buttonLayerUp.UseVisualStyleBackColor = true;
+			this.buttonLayerUp.Click += new System.EventHandler(this.buttonLayerUp_Click);
+			// 
+			// labelFrameInfo
+			// 
+			this.labelFrameInfo.AutoSize = true;
+			this.labelFrameInfo.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.labelFrameInfo.Location = new System.Drawing.Point(0, 413);
+			this.labelFrameInfo.Name = "labelFrameInfo";
+			this.labelFrameInfo.Size = new System.Drawing.Size(55, 20);
+			this.labelFrameInfo.TabIndex = 16;
+			this.labelFrameInfo.Text = "Frame";
+			// 
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(430, 358);
+			this.ClientSize = new System.Drawing.Size(724, 619);
+			this.Controls.Add(this.panelLayerGrid);
+			this.Controls.Add(this.panelLayerOptions);
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
-			this.Name = "MainWindow";
+			this.Name = "MainForm";
 			this.Text = "LED Cube Utility";
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
+			this.panelLayerOptions.ResumeLayout(false);
+			this.panelLayerOptions.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -196,14 +303,22 @@
 		private System.Windows.Forms.ToolStripMenuItem mnuFileSaveAs;
 		private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem mnuEditNewFrame;
-		private System.Windows.Forms.ToolStripMenuItem mnuEditFrame;
-		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
-		private System.Windows.Forms.ToolStripMenuItem mnuEditFillLayer;
-		private System.Windows.Forms.ToolStripMenuItem mnuEditClearLayer;
 		private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem mnuToolsComPort;
 		private System.Windows.Forms.ToolStripMenuItem mnuToolsBaudRate;
 		private System.Windows.Forms.ToolStripMenuItem mnuToolsProgramDevice;
+		private System.Windows.Forms.Panel panelLayerOptions;
+		private System.Windows.Forms.ToolStripMenuItem mnuEditDeleteFrame;
+		private System.Windows.Forms.Panel panelLayerGrid;
+		private System.Windows.Forms.Label labelFrameInfo;
+		private System.Windows.Forms.Button buttonClearLayer;
+		private System.Windows.Forms.Button buttonFillLayer;
+		private System.Windows.Forms.Button buttonLayerDown;
+		private System.Windows.Forms.Button buttonLayerUp;
+		private System.Windows.Forms.Label labelLayerInfo;
+		private System.Windows.Forms.Button buttonFrameRight;
+		private System.Windows.Forms.Button buttonFrameLeft;
+		private System.Windows.Forms.Button buttonFrameTime;
 	}
 }
 
--- LED Cube Utility/MainForm.cs
+++ LED Cube Utility/MainForm.cs
@@ -13,7 +13,11 @@ namespace LED_Cube_Utility {
 	public partial class MainForm : Form {
 		private string AnimationFilePath = null;
 		private Led3dAnimation Animation = null;
-		private int CurrentAnimationLayer = 0;
+		private int AnimationFrameIndex = -1;
+		private Led3dFrame AnimationFrame = null;
+		private int AnimationLayerIndex = -1;
+		private Led3dFrameLayer AnimationLayer = null;
+		private List<Button> PixelButtons;
 
 		public MainForm() {
 			InitializeComponent();
@@ -25,12 +29,85 @@ namespace LED_Cube_Utility {
 			errorForm.Show();
 		}
 
+		// (!) Change to refreshAnimationLayer, move setup code to new function (called by File>New), store buttons in global list
+		public void ChangeAnimationLayer(int layerIndex) {
+			labelLayerInfo.Text = "Layer " + (layerIndex + 1) + "/" + Animation.Height;
+			AnimationLayer = AnimationFrame.GetLed3dFrameLayer(layerIndex);
+			// set up the tick boxes
+
+			int panelWidth = panelLayerGrid.Width;
+			int panelHeight = panelLayerGrid.Height;
+
+			int borderWidthHorizontal = 10;
+			int borderWidthVertical = 10;
+			int paddingHorizontal = 5;
+			int paddingVertical = 5;
+
+			int startingX = borderWidthHorizontal;
+			int startingY = borderWidthVertical;
+
+			int unitsHorizontal = (int)AnimationLayer.Width;
+			int unitsVertical = (int)AnimationLayer.Length;
+
+			int unitWidth = (panelWidth - 2 * borderWidthHorizontal - (unitsHorizontal - 1) * paddingHorizontal) / unitsHorizontal;
+			int unitHeight = (panelHeight - 2 * borderWidthVertical - (unitsVertical - 1) * paddingVertical) / unitsVertical;
+
+			Color buttonSetColor = Color.Black;
+			Color buttonNotSetColor = Color.White;
+
+			int y = startingY;
+			for (int verticalIndex = 0; verticalIndex < unitsVertical; verticalIndex++) {
+				int x = startingX;
+				for (int horizontalIndex = 0; horizontalIndex < unitsHorizontal; horizontalIndex++) {
+					Button b = new Button();
+					b.Name = "Pixel_" + horizontalIndex + "_" + verticalIndex;
+					b.Click += new EventHandler(PixelClickHandler);
+					b.Text = "";
+					b.Width = unitWidth;
+					b.Height = unitHeight;
+					b.Location = new Point(x, y);
+
+					Color buttonColor;
+					if (AnimationLayer.GetPixel(horizontalIndex, verticalIndex)) {
+						buttonColor = buttonSetColor;
+					} else {
+						buttonColor = buttonNotSetColor;
+					}
+					b.BackColor = buttonColor;
+
+					panelLayerGrid.Controls.Add(b);
+
+					x += unitWidth + paddingHorizontal;
+				}
+				y += unitHeight + paddingVertical;
+			}
+
+		}
+
+		public void ChangeAnimationFrame(int frameIndex) {
+			labelFrameInfo.Text = "Frame " + (frameIndex + 1) + "/" + Animation.GetFrameCount();
+			AnimationFrameIndex = frameIndex;
+			AnimationLayerIndex = 0;
+			AnimationFrame = Animation.GetFrame(AnimationFrameIndex);
+			ChangeAnimationLayer(AnimationLayerIndex);
+		}
+
+		public void PixelClickHandler(Object sender, EventArgs e) {
+			ErrorPopUp(((Button)sender).Name + " was clicked");
+		}
+
 		private void mnuFileNew_Click(object sender, EventArgs e) {
 			var dimensionForm = new NewAnimationForm();
 			dimensionForm.ShowDialog();
 
 			if (dimensionForm.DialogResult == DialogResult.OK) {
 				Animation = new Led3dAnimation(dimensionForm.Width, dimensionForm.Length, dimensionForm.Height);
+				AnimationFrameIndex = 0;
+				AnimationFrame = Animation.GetFrame(AnimationFrameIndex);
+				AnimationLayerIndex = 0;
+
+				ChangeAnimationFrame(AnimationFrameIndex);
+				ChangeAnimationLayer(AnimationLayerIndex);
 			}			
 		}
 
@@ -65,31 +142,8 @@ namespace LED_Cube_Utility {
 			ErrorPopUp("Not implemented.");
 		}
 
-		private void mnuEditFrame_Click(object sender, EventArgs e) {
-			if (Animation == null) {
-				ErrorPopUp("No animation active.");
-				return;
-			}
+		private void mnuEditDeleteFrame_Click(object sender, EventArgs e) {
 
-			ErrorPopUp("Not implemented.");
-		}
-
-		private void mnuEditFillLayer_Click(object sender, EventArgs e) {
-			if (Animation == null) {
-				ErrorPopUp("No animation active.");
-				return;
-			}
-
-			ErrorPopUp("Not implemented.");
-		}
-
-		private void mnuEditClearLayer_Click(object sender, EventArgs e) {
-			if (Animation == null) {
-				ErrorPopUp("No animation active.");
-				return;
-			}
-
-			ErrorPopUp("Not implemented.");
 		}
 
 		private void mnuToolsComPort_Click(object sender, EventArgs e) {
@@ -102,6 +156,64 @@ namespace LED_Cube_Utility {
 
 		private void mnuToolsProgramDevice_Click(object sender, EventArgs e) {
 			ErrorPopUp("Not implemented.");
+		}
+
+		
+
+		private void buttonLayerUp_Click(object sender, EventArgs e) {
+			if (Animation == null) {
+				ErrorPopUp("No animation active.");
+				return;
+			}
+
+			int resultingLayer = AnimationLayerIndex + 1;
+			if (resultingLayer < Animation.Height) {
+				AnimationLayerIndex++;
+				ChangeAnimationLayer(AnimationLayerIndex);
+			}
+		}
+
+		private void buttonLayerDown_Click(object sender, EventArgs e) {
+			if (Animation == null) {
+				ErrorPopUp("No animation active.");
+				return;
+			}
+
+			int resultingLayer = AnimationLayerIndex - 1;
+			if (resultingLayer >= 0) {
+				AnimationLayerIndex--;
+				ChangeAnimationLayer(AnimationLayerIndex);
+			}
+		}
+
+		private void buttonFillLayer_Click(object sender, EventArgs e) {
+			if (Animation == null) {
+				ErrorPopUp("No animation active.");
+				return;
+			}
+
+			ErrorPopUp("Not implemented.");
+		}
+
+		private void buttonClearLayer_Click(object sender, EventArgs e) {
+			if (Animation == null) {
+				ErrorPopUp("No animation active.");
+				return;
+			}
+
+			ErrorPopUp("Not implemented.");
+		}
+
+		private void buttonFrameTime_Click(object sender, EventArgs e) {
+
+		}
+
+		private void buttonFrameLeft_Click(object sender, EventArgs e) {
+
+		}
+
+		private void buttonFrameRight_Click(object sender, EventArgs e) {
+
 		}
 	}
 }
